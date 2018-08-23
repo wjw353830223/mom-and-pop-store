58bc43900a1d3b4d3f08892a4041058a1dff80ad
caoruipeng <caoruipeng94@126.com>
2018-08-23 09:40:42 +0800

    前端

--- router.php
+++ router.php
@@ -2,23 +2,16 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+// $Id$
 
-// [ 应用入口文件 ]
-if(!file_exists(__DIR__ . '/../data/install.lock')){
-    define('BIND_MODULE', 'install');
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+    return false;
+} else {
+    require __DIR__ . "/index.php";
 }
-
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// 定义应用缓存目录
-define('RUNTIME_PATH', __DIR__ . '/../runtime/');
-// 开启调试模式
-define('APP_DEBUG', true);
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/26
+ * Time: 16:53
+ */
+
+ echo 8|3;
